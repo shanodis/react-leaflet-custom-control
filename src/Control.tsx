0375478dd30dd2b83f3ae930d955d53dad21e8e6
Dominik Grudzień <dominik.grudzien@silevis.com>
2022-09-05 13:58:14 +0200

    added containerId

--- src/Control.tsx
+++ src/Control.tsx
@@ -6,25 +6,27 @@ interface Props {
   position: L.ControlPosition
   children?: React.ReactNode
   container?: React.HTMLAttributes<HTMLDivElement>
+  containerId?: string | number
   prepend?: boolean
 }
 
-const POSITION_CLASSES = {
-  bottomleft: 'leaflet-bottom leaflet-left',
-  bottomright: 'leaflet-bottom leaflet-right',
-  topleft: 'leaflet-top leaflet-left',
-  topright: 'leaflet-top leaflet-right',
+const POSITION_SELECTORS = {
+  bottomleft: '.leaflet-bottom.leaflet-left',
+  bottomright: '.leaflet-bottom.leaflet-right',
+  topleft: '.leaflet-top.leaflet-left',
+  topright: '.leaflet-top.leaflet-right',
 }
 
 const Control = (props: Props): JSX.Element => {
   const [portalRoot, setPortalRoot] = React.useState<any>(document.createElement('div'))
-  const positionClass = ((props.position && POSITION_CLASSES[props.position]) || POSITION_CLASSES.topright)
+  const positionSelector = ((props.position && POSITION_SELECTORS[props.position]) || POSITION_SELECTORS.topright)
   const portalContainer = document.createElement('div')
 
   React.useEffect(() => {
-    const targetDiv = document.getElementsByClassName(positionClass)
+    const querySelectors = `${props?.containerId && `#${props?.containerId}`} ${positionSelector}`.trim()
+    const targetDiv = document.querySelectorAll(querySelectors)
     setPortalRoot(targetDiv[0])
-  }, [positionClass])
+  }, [positionSelector, props?.containerId])
 
   if (props.prepend !== undefined && props.prepend === true) {
     portalRoot.prepend(portalContainer)
